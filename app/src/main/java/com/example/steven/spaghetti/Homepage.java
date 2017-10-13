7d8b7d331aa88be30c2303b594b0177e783bbd59
harwinsetyawan <harwinn@live.com>
2017-10-14 10:31:43 +1100

    it works

--- app/src/main/java/com/example/steven/spaghetti/Homepage.java
+++ app/src/main/java/com/example/steven/spaghetti/Homepage.java
@@ -93,7 +93,7 @@ public class Homepage extends AppCompatActivity {
         } return true;
     }
 
-    public void onClick(){
+    public void onClick(View view){
         Intent i = new Intent(this, forumForm.class);
         startActivity(i);
     }
